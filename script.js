daf8b5c50ff0e9ecd8b311e3628b0f3327db34d4
wasemalwisy <31485227+wasemalwisy@users.noreply.github.com>
2022-08-02 19:57:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ function ip() {
     document.getElementById("hd").style.backgroundRepeat = "no-repeat";
     document.getElementById("hd").style.backgroundSize = "cover"; 
     document.getElementById("hd").style.backgroundPosition = "center";
-    document.getElementById("Model").innerHTML="Iphone"; 
-   
+    document.getElementById("Model").innerHTML="Iphone";  
+
+    document.getElementById("info").style.color="#fff"; 
+    document.getElementById("line").style.background="#fff"; 
+    document.getElementById("Model").style.color="#fff"; 
+    coloWhite();
      
 //   x1();
 }
@@ -17,16 +21,32 @@ function s() {
     document.getElementById("hd").style.backgroundPosition = "center";
     document.getElementById("Model").innerHTML="Samsung"; 
     var x =  document.getElementById("samsung");   
+
+    document.getElementById("info").style.color="black"; 
+    document.getElementById("line").style.background="black"; 
+    document.getElementById("Model").style.color="black"; 
+
+    coloWhite();
     // x2();
 }
      
 
-function o() {   document.getElementById("hd").style.background  = "url(Oppo.jpg)"; 
+function o() {   document.getElementById("hd").style.background  = "url(oppo2.jpg)"; 
 document.getElementById("hd").style.backgroundRepeat = "no-repeat";
 document.getElementById("hd").style.backgroundSize = "cover"; 
 document.getElementById("hd").style.backgroundPosition = "center";
-    document.getElementById("Model").innerHTML="OPPO"; 
-    var x=  document.getElementById("OPPO");  
+
+// document. getElementsByClassName("black").style.color = "black";
+var x1=  document.getElementById("iphone");  
+var x2=  document.getElementById("Samsung");  
+var x3=  document.getElementById("OPPO");  
+var x4=  document.getElementById("Nokia");  
+var x5=  document.getElementById("Huawei");  
+
+coloBlack();
+   
+document.getElementById("Model").innerHTML="OPPO"; 
+    
     // x3();
      
 }
@@ -37,6 +57,12 @@ function n() {
     document.getElementById("hd").style.backgroundSize = "cover"; 
     document.getElementById("hd").style.backgroundPosition = "center";
     document.getElementById("Model").innerHTML="Nokia"; 
+    
+    document.getElementById("info").style.color="#fff"; 
+    document.getElementById("line").style.background="#fff"; 
+    document.getElementById("Model").style.color="#fff"; 
+    coloWhite();
+
     //    x4();
     
 }
@@ -47,6 +73,12 @@ document.getElementById("hd").style.backgroundRepeat = "no-repeat";
 document.getElementById("hd").style.backgroundSize = "cover"; 
 document.getElementById("hd").style.backgroundPosition = "center";
     document.getElementById("Model").innerHTML="Huawei"; 
+
+    
+    document.getElementById("info").style.color="black"; 
+    document.getElementById("line").style.background="black"; 
+    document.getElementById("Model").style.color="#fff"; 
+
     //  x5();
     
 }
@@ -128,3 +160,36 @@ document.getElementById("hd").style.backgroundPosition = "center";
     if (x.style.display === "block") {x.style.display = "none"; }  
     else {x.style.display = "none"; } 
 }
+
+function coloBlack(){
+var x1=  document.getElementById("iphone");  
+var x2=  document.getElementById("Samsung");  
+var x3=  document.getElementById("OPPO");  
+var x4=  document.getElementById("Nokia");  
+var x5=  document.getElementById("Huawei");  
+
+document.getElementById("info").style.color="black"; 
+document.getElementById("line").style.background="black"; 
+document.getElementById("Model").style.color="black"; 
+x1.style.color="black"; 
+x2.style.color="black"; 
+x3.style.color="black"; 
+x4.style.color="black"; 
+x5.style.color="black"; 
+}
+
+function coloWhite(){
+    var w="#fff";
+    var x1=  document.getElementById("iphone");  
+    var x2=  document.getElementById("Samsung");  
+    var x3=  document.getElementById("OPPO");  
+    var x4=  document.getElementById("Nokia");  
+    var x5=  document.getElementById("Huawei");  
+     
+    x1.style.color=w; 
+    x2.style.color=w; 
+    x3.style.color=w; 
+    x4.style.color=w; 
+    x5.style.color=w; 
+    }
+    
